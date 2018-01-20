b71376c2f18b5f50db142651bf0bac6477ad7e3b
rikachka <rikachka1@gmail.com>
2018-01-20 17:28:28 +0300

    Refactored structure of data

--- dst_html/components/timetable.js
+++ dst_html/components/timetable.js
@@ -9,45 +9,37 @@ var day_length = day_end - day_begin;
 
 var meeting_selected = 0;
 
-var availability1 = [
-    {start: 12 * 5 + day_begin, end: 30 * 5 + day_begin, meeting_id: 2},
-    {start: 90 * 5 + day_begin, end: day_end, meeting_id: 4},
+var floors_number = 8;
+
+var meetings = [ 
+    {meeting_id: 6, title: 'Собрание 6', start: 0 * 5 + day_begin, end: day_end, date: '14 декабря, 15:00—17:00', participant: 'Дарт Вейдер', number: 12, room_id: 1},
+    {meeting_id: 2, title: 'Собрание 2', start: 12 * 5 + day_begin, end: 30 * 5 + day_begin, date: '14 декабря, 15:00—17:00', participant: 'Дарт Вейдер', number: 12, room_id: 2},
+    {meeting_id: 4, title: 'Собрание 4', start: 90 * 5 + day_begin, end: day_end, date: '14 декабря, 15:00—17:00', participant: 'Дарт Вейдер', number: 12, room_id: 2},
+    {meeting_id: 3, title: 'Собрание 3', start: 70 * 5 + day_begin, end: 90 * 5 + day_begin, date: '14 декабря, 15:00—17:00', participant: 'Дарт Вейдер', number: 12, room_id: 3},
+    {meeting_id: 1, title: 'Собрание 1', start: 0 * 5 + day_begin, end: 50 * 5 + day_begin, date: '14 декабря, 15:00—17:00', participant: 'Дарт Вейдер', number: 12, room_id: 3},
+    {meeting_id: 5, title: 'Собрание 5', start: 120 * 5 + day_begin, end: 168 * 5 + day_begin, date: '14 декабря, 15:00—17:00', participant: 'Дарт Вейдер', number: 12, room_id: 3},
+    {meeting_id: 7, title: 'Собрание 7', start: 0 * 5 + day_begin, end: day_end, date: '14 декабря, 15:00—17:00', participant: 'Дарт Вейдер', number: 12, room_id: 4},
+    {meeting_id: 8, title: 'Собрание 8', start: 0 * 5 + day_begin, end: day_end, date: '14 декабря, 15:00—17:00', participant: 'Дарт Вейдер', number: 12, room_id: 5},
+    {meeting_id: 9, title: 'Собрание 9', start: 0 * 5 + day_begin, end: day_end, date: '14 декабря, 15:00—17:00', participant: 'Дарт Вейдер', number: 12, room_id: 8},
+    {meeting_id: 10, title: 'Собрание 10', start: 0 * 5 + day_begin, end: day_end, date: '14 декабря, 15:00—17:00', participant: 'Дарт Вейдер', number: 12, room_id: 9},
 ];
 
-var availability2 = [
-    {start: 0 * 5 + day_begin, end: 50 * 5 + day_begin, meeting_id: 1},
-    {start: 70 * 5 + day_begin, end: 90 * 5 + day_begin, meeting_id: 3},
-    {start: 120 * 5 + day_begin, end: 168 * 5 + day_begin, meeting_id: 5},
-];
-
-var availability3 = [
-    {start: 0 * 5 + day_begin, end: day_end, meeting_id: 6},
-];
-
-var meeting1 = {
-    meeting_id: 4,
-    title: 'Рассуждения о высоком',
-    date: '14 декабря, 15:00—17:00',
-    participant_example: 'Дарт Вейдер',
-    participants_number: 12,
-}
-
-var rooms7 = [
-    {name: 'Ржавый Фред', capacity: 6, room_id: 1, room_disabled: 'true', availability: buildMinutesAvailability(availability3), },
-    {name: 'Прачечная', capacity: 10, room_id: 2, availability: buildMinutesAvailability(availability1), },
-    {name: 'Желтый дом', capacity: 10, room_id: 3, availability: buildMinutesAvailability(availability2), },
-    {name: 'Оранжевый тюльпан', capacity: 6, room_id: 4, room_disabled: 'true', availability: buildMinutesAvailability(availability3), },
-];
-
-var rooms6 = [
-    {name: 'Джокер', capacity: 6, room_id: 5, room_disabled: 'true', availability: buildMinutesAvailability(availability3), },
-    {name: 'Мариванна', capacity: 10, room_id: 6, availability: buildMinutesAvailability(availability2), },
-    {name: 'Тонкий Боб', capacity: 10, room_id: 7, availability: buildMinutesAvailability(availability1), },
-    {name: 'Черная вдова', capacity: 6, room_id: 8, room_disabled: 'true', availability: buildMinutesAvailability(availability3), },
-    {name: 'Белорусский ликер', capacity: 6, room_id: 9, room_disabled: 'true', availability: buildMinutesAvailability(availability3), },
+var rooms = [
+    {name: 'Ржавый Фред', capacity: 6, room_id: 1, room_disabled: 'true', floor: 7},
+    {name: 'Прачечная', capacity: 10, room_id: 2, floor: 7},
+    {name: 'Желтый дом', capacity: 10, room_id: 3, floor: 7},
+    {name: 'Оранжевый тюльпан', capacity: 6, room_id: 4, room_disabled: 'true', floor: 7},
+    {name: 'Джокер', capacity: 6, room_id: 5, room_disabled: 'true', floor: 6},
+    {name: 'Мариванна', capacity: 10, room_id: 6, floor: 6},
+    {name: 'Тонкий Боб', capacity: 10, room_id: 7, floor: 6},
+    {name: 'Черная вдова', capacity: 6, room_id: 8, room_disabled: 'true', floor: 6 },
+    {name: 'Белорусский ликер', capacity: 6, room_id: 9, room_disabled: 'true', floor: 6},
 ];
 
 function buildMinutesAvailability(meetings) {
+    meetings = meetings.sort(function(meeting1, meeting2) {
+        return meeting1.start - meeting2.start;
+    });
     var availability = [];
     var cur_minute = day_begin;
     var earlier_today = getTime().cur_shift;
@@ -55,16 +47,20 @@ function buildMinutesAvailability(meetings) {
         while (cur_minute < meeting.start) {
             if (cur_minute < earlier_today) {
                 let block_end = Math.min(meeting.start, earlier_today);
-                availability.push({start: cur_minute, length: (block_end - cur_minute) * 100 / day_length, occupied: true, meeting: meeting1});
+                availability.push({start: cur_minute, length: (block_end - cur_minute) * 100 / day_length, occupied: true, meeting: meeting});
                 cur_minute = block_end;
             } else {
                 availability.push({start: cur_minute, length: (meeting.start - cur_minute) * 100 / day_length});
                 cur_minute = meeting.start;
             }
         }
-        availability.push({start: cur_minute, length: (meeting.end - meeting.start) * 100 / day_length, meeting_id: meeting.meeting_id, occupied: true, meeting: meeting1});
+        availability.push({start: cur_minute, length: (meeting.end - meeting.start) * 100 / day_length, meeting_id: meeting.meeting_id, occupied: true, meeting: meeting});
         cur_minute = meeting.end;
     });
+    if (cur_minute < earlier_today) {       
+        availability.push({start: cur_minute, length: (earlier_today - cur_minute) * 100 / day_length, occupied: true});
+        cur_minute = earlier_today;
+    } 
     availability.push({start: cur_minute, length: (day_end - cur_minute) * 100 / day_length});
     return availability;
 };
@@ -99,7 +95,7 @@ function determineHoursState(now_hour) {
 
 function getTime() {
     let date = new Date();
-    date.setHours(date.getHours() - 3);
+    date.setHours(date.getHours() - 7);
     let hour = date.getHours();
     let minutes = date.getMinutes();
     let shift = hour * 60 + minutes;
@@ -114,6 +110,23 @@ function getTime() {
     };
 }
 
+function constructRooms() {
+    var floors = [];
+    for (let index = 0; index < floors_number; ++index) {
+        floors[index] = {}
+        floors[index].number = index + 1;
+        floors[index].rooms = [];
+    }
+    rooms.forEach(function(room) {
+        var room_meetings = meetings.filter(function(meeting) {
+            return meeting.room_id == room.room_id;
+        });
+        room.availability = buildMinutesAvailability(room_meetings);
+        floors[room.floor - 1].rooms.push(room);
+    });
+    return floors.reverse();
+}
+
 export default Ractive.extend({
     template: template.template,
     css: template.css,
@@ -121,13 +134,10 @@ export default Ractive.extend({
         ComponentMeetingPopup: ComponentMeetingPopup
     },
     data: {
-        floors: [
-            {number: 7, rooms: rooms7},
-            {number: 6, rooms: rooms6},
-        ],
         meeting_selected: 0,
     },
     computed: {
+        floors: constructRooms,
         time: getTime,
     },
     oncomplete: function() {

cbd38d0f339617c05b1766486dc8600d1db6b5aa
rikachka <rikachka1@gmail.com>
2018-01-20 16:04:43 +0300

    Added positioning of meeting-popup and information properties

--- dst_html/components/timetable.js
+++ dst_html/components/timetable.js
@@ -3,20 +3,34 @@ var template = require('./timetable.haml');
 var dateFormat = require('dateformat');
 import ComponentMeetingPopup from '../components/meeting_popup.js';
 
+var day_begin = 8 * 60;
+var day_end = 23 * 60;
+var day_length = day_end - day_begin;
+
+var meeting_selected = 0;
+
 var availability1 = [
-    {start: 12, end: 30, meeting_id: 2},
-    {start: 90, end: 180, meeting_id: 4},
+    {start: 12 * 5 + day_begin, end: 30 * 5 + day_begin, meeting_id: 2},
+    {start: 90 * 5 + day_begin, end: day_end, meeting_id: 4},
 ];
 
 var availability2 = [
-    {start: 0, end: 50, meeting_id: 1},
-    {start: 70, end: 90, meeting_id: 3},
-    {start: 120, end: 168, meeting_id: 5},
+    {start: 0 * 5 + day_begin, end: 50 * 5 + day_begin, meeting_id: 1},
+    {start: 70 * 5 + day_begin, end: 90 * 5 + day_begin, meeting_id: 3},
+    {start: 120 * 5 + day_begin, end: 168 * 5 + day_begin, meeting_id: 5},
 ];
 
 var availability3 = [
-    {start: 0, end: 180, meeting_id: 6},
+    {start: 0 * 5 + day_begin, end: day_end, meeting_id: 6},
 ];
+
+var meeting1 = {
+    meeting_id: 4,
+    title: 'Рассуждения о высоком',
+    date: '14 декабря, 15:00—17:00',
+    participant_example: 'Дарт Вейдер',
+    participants_number: 12,
+}
 
 var rooms7 = [
     {name: 'Ржавый Фред', capacity: 6, room_id: 1, room_disabled: 'true', availability: buildMinutesAvailability(availability3), },
@@ -35,27 +49,28 @@ var rooms6 = [
 
 function buildMinutesAvailability(meetings) {
     var availability = [];
-    var cur_minute = 0;
+    var cur_minute = day_begin;
     var earlier_today = getTime().cur_shift;
     meetings.forEach((meeting) => {
-        for(; cur_minute < meeting.start; cur_minute++) {
+        while (cur_minute < meeting.start) {
             if (cur_minute < earlier_today) {
-                availability.push({minute: cur_minute, occupied: true});
+                let block_end = Math.min(meeting.start, earlier_today);
+                availability.push({start: cur_minute, length: (block_end - cur_minute) * 100 / day_length, occupied: true, meeting: meeting1});
+                cur_minute = block_end;
             } else {
-                availability.push({minute: cur_minute});
+                availability.push({start: cur_minute, length: (meeting.start - cur_minute) * 100 / day_length});
+                cur_minute = meeting.start;
             }
         }
-        for(; cur_minute < meeting.end; cur_minute++) {
-            availability.push({minute: cur_minute, meeting_id: meeting.meeting_id, occupied: true});
-        }
+        availability.push({start: cur_minute, length: (meeting.end - meeting.start) * 100 / day_length, meeting_id: meeting.meeting_id, occupied: true, meeting: meeting1});
+        cur_minute = meeting.end;
     });
-    for(; cur_minute < 180; cur_minute++) {
-        availability.push({minute: cur_minute});
-    }
+    availability.push({start: cur_minute, length: (day_end - cur_minute) * 100 / day_length});
     return availability;
 };
 
 function buildMinutesBlocks(now_block) {
+    now_block = Math.floor((now_block - day_begin) / 5);
     var blocks = [];
     var cur_minute = 0;
     for(; cur_minute < 180; cur_minute++) {
@@ -84,10 +99,10 @@ function determineHoursState(now_hour) {
 
 function getTime() {
     let date = new Date();
-    date.setHours(date.getHours() - 10);
+    date.setHours(date.getHours() - 3);
     let hour = date.getHours();
     let minutes = date.getMinutes();
-    let shift = Math.floor((hour * 60 + minutes - 8 * 60) / 5);
+    let shift = hour * 60 + minutes;
     let time = dateFormat(date, 'HH:MM');
     return {
         now: time, 
@@ -105,38 +120,44 @@ export default Ractive.extend({
     components: {
         ComponentMeetingPopup: ComponentMeetingPopup
     },
-    data: function() {
-        return {
-            floors: [
-                {number: 7, rooms: rooms7},
-                {number: 6, rooms: rooms6},
-            ],
-        }
+    data: {
+        floors: [
+            {number: 7, rooms: rooms7},
+            {number: 6, rooms: rooms6},
+        ],
+        meeting_selected: 0,
     },
     computed: {
         time: getTime,
     },
     oncomplete: function() {
-    	$('.minute').click((item) => {
+    	$('.meeting').click((item) => {
             if (item.target.attributes.meeting) {
                 let meeting_id = item.target.attributes.meeting.value;
-                $('.minute[meeting!="' + meeting_id + '"]').removeAttr('meeting_clicked');
-                $('.minute[meeting="' + meeting_id + '"]').attr('meeting_clicked', true);
                 let room_id = item.target.attributes.room.value;
-                $('.calendar-rooms[room!="' + room_id + '"]').children('.room-name').removeAttr('room_clicked');
-                $('.calendar-rooms[room="' + room_id + '"]').children('.room-name').attr('room_clicked', 'true');
+                if (item.target.attributes.meeting_clicked) {
+                    this.set('meeting_selected', 0);
+                    $('.meeting[meeting="' + meeting_id + '"]').removeAttr('meeting_clicked');
+                    $('.calendar-rooms[room="' + room_id + '"]').children('.room-name').removeAttr('room_clicked');
+                } else {
+                    this.set('meeting_selected', meeting_id);
+                    $('.meeting[meeting!="' + meeting_id + '"]').removeAttr('meeting_clicked');
+                    $('.meeting[meeting="' + meeting_id + '"]').attr('meeting_clicked', true);
+                    $('.calendar-rooms[room!="' + room_id + '"]').children('.room-name').removeAttr('room_clicked');
+                    $('.calendar-rooms[room="' + room_id + '"]').children('.room-name').attr('room_clicked', 'true');
+                }
             }
     	});
         
-    	$('.minute').hover((item) => {
+    	$('.meeting').hover((item) => {
             if (item.target.attributes.meeting) {
                 let meeting_id = item.target.attributes.meeting.value;
-                $('.minute[meeting="' + meeting_id + '"]').attr('meeting_hover', true);
+                $('.meeting[meeting="' + meeting_id + '"]').attr('meeting_hover', true);
                 let room_id = item.target.attributes.room.value;
                 $('.calendar-rooms[room="' + room_id + '"]').children('.room-name').attr('room_hover', 'true');
             }
     	}, () => {
-            $('.minute').removeAttr('meeting_hover');
+            $('.meeting').removeAttr('meeting_hover');
             $('.calendar-rooms').children('.room-name').removeAttr('room_hover');
         });
     }
